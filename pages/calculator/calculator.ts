f3d9513fc6749c2131548076038589655f23e017
KyleCo03 <irvin.co180367@my.lsgh.edu.ph>
2021-05-02 16:15:30 +0800

    1 file modified

--- pages/calculator/calculator.ts
+++ pages/calculator/calculator.ts
@@ -18,7 +18,23 @@ export class CalculatorPage {
 
   constructor(public navCtrl: NavController) {}
 
-  
+    calculateIBW()
+    {
+      this.tothii = (this.hif * 12) + this.hii;
+
+      if (this.gender = "male") {
+        this.miller   = 56.2 + ((this.tothii - 60) * 1.41);
+        this.robinson = 52   + ((this.tothii - 60) * 1.9 );
+        this.devine   = 50   + ((this.tothii - 60) * 2.3 );
+        this.hamwi    = 48   + ((this.tothii - 60) * 2.7 );
+      } else {
+        this.miller   = 53.1 + ((this.tothii - 60) * 1.36);
+        this.robinson = 49   + ((this.tothii - 60) * 1.7 );
+        this.devine   = 45.5 + ((this.tothii - 60) * 2.3 );
+        this.hamwi    = 45.5 + ((this.tothii - 60) * 2.2 );
+      }
+      
+    }
 
 
 
