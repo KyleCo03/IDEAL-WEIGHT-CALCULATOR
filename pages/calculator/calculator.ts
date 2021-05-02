f986b2de0fadee888253bf621489416da9fbc73b
KyleCo03 <irvin.co180367@my.lsgh.edu.ph>
2021-05-02 23:27:07 +0800

    2 files modified

--- pages/calculator/calculator.ts
+++ pages/calculator/calculator.ts
@@ -28,15 +28,24 @@ export class CalculatorPage {
         if (this.gender = "m") 
         {
           this.miller   = 56.2 + ((this.tothii - 60) * 1.41);
+          this.miller   = parseFloat(this.miller.toFixed(2));
           this.robinson = 52   + ((this.tothii - 60) * 1.9 );
+          this.robinson = parseFloat(this.robinson.toFixed(2));
           this.devine   = 50   + ((this.tothii - 60) * 2.3 );
+          this.devine   = parseFloat(this.devine.toFixed(2));
           this.hamwi    = 48   + ((this.tothii - 60) * 2.7 );
+          this.hamwi    = parseFloat(this.hamwi.toFixed(2));
+
         } else 
           {
             this.miller   = 53.1 + ((this.tothii - 60) * 1.36);
+            this.miller   = parseFloat(this.miller.toFixed(2));
             this.robinson = 49   + ((this.tothii - 60) * 1.7 );
+            this.robinson = parseFloat(this.robinson.toFixed(2));
             this.devine   = 45.5 + ((this.tothii - 60) * 2.3 );
-            this.hamwi    = 45.5 + ((this.tothii - 60) * 2.2 );         
+            this.devine   = parseFloat(this.devine.toFixed(2));
+            this.hamwi    = 45.5 + ((this.tothii - 60) * 2.2 );
+            this.hamwi    = parseFloat(this.hamwi.toFixed(2));         
           }
       } 
       else
@@ -44,6 +53,15 @@ export class CalculatorPage {
         this.IBWmessage="Minimum height of 5 feet please"
       }
     }
+
+    test(event) 
+    {
+      this.miller = 0;
+      this.robinson = 0;
+      this.devine = 0;
+      this.hamwi = 0;
+    }
+
 
 }
     
