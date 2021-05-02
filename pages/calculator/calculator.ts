4f95f0bd139b5b8243598457aab92c8ebdfb45c1
KyleCo03 <irvin.co180367@my.lsgh.edu.ph>
2021-05-02 19:55:18 +0800

    2 files modified

--- pages/calculator/calculator.ts
+++ pages/calculator/calculator.ts
@@ -15,6 +15,7 @@ export class CalculatorPage {
   robinson: number;
   devine: number;
   hamwi: number;
+  IBWmessage: string;
 
   constructor(public navCtrl: NavController) {}
 
@@ -22,22 +23,33 @@ export class CalculatorPage {
     {
       this.tothii = (this.hif * 12) + this.hii;
 
-      if (this.gender = "male") {
-        this.miller   = 56.2 + ((this.tothii - 60) * 1.41);
-        this.robinson = 52   + ((this.tothii - 60) * 1.9 );
-        this.devine   = 50   + ((this.tothii - 60) * 2.3 );
-        this.hamwi    = 48   + ((this.tothii - 60) * 2.7 );
-      } else {
-        this.miller   = 53.1 + ((this.tothii - 60) * 1.36);
-        this.robinson = 49   + ((this.tothii - 60) * 1.7 );
-        this.devine   = 45.5 + ((this.tothii - 60) * 2.3 );
-        this.hamwi    = 45.5 + ((this.tothii - 60) * 2.2 );
+      if(this.tothii >= 60)
+      {
+        if (this.gender = "male") 
+        {
+          this.miller   = 56.2 + ((this.tothii - 60) * 1.41);
+          this.robinson = 52   + ((this.tothii - 60) * 1.9 );
+          this.devine   = 50   + ((this.tothii - 60) * 2.3 );
+          this.hamwi    = 48   + ((this.tothii - 60) * 2.7 );
+        } else 
+          {
+            this.miller   = 53.1 + ((this.tothii - 60) * 1.36);
+            this.robinson = 49   + ((this.tothii - 60) * 1.7 );
+            this.devine   = 45.5 + ((this.tothii - 60) * 2.3 );
+            this.hamwi    = 45.5 + ((this.tothii - 60) * 2.2 );         
+          }
+      } 
+      else
+      {
+        this.IBWmessage="Minimum height of 5 feet please"
       }
-      
     }
+
+}
+    
 
 
 
   
 
-}
+
